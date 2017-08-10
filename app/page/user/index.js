9b5bebcc31c55f885ae3a19a2d399954a3f03dfa
EwingYangs <jiaying.yang@qq.com>
2017-08-10 23:12:31 +0800

    user

---
+++ app/page/user/index.js
@@ -0,0 +1,72 @@
+// page/user/index.js
+var Bmob = require("../../utils/bmob.js");
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    var currentUser = Bmob.User.current();
+    var userInfo = currentUser.attributes;
+    this.setData({
+      userInfo: userInfo,
+    })  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
