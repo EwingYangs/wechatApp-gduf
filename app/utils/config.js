2f067cdd71d4a47ceea739ab6659b6432845d84d
yangjiaying <jiaying.yang@qq.com>
2018-03-13 18:01:16 +0800

    hosts

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,14 +1,15 @@
+var host = "https://www.itbasket.top";
 module.exports = {
   // API 接口www.gduf-api.com
   token: "bad0f20dbd034357e6bff27e7a4453b3",
-  gdufLoginUrl: "http://www.gduf-api.com/site/login",//登录接口
-  gdufScoreUrl: "http://www.gduf-api.com/site/score",//查询成绩接口
-  gdufRoomUrl: "http://www.gduf-api.com/room/get-room",//查询自习室接口
-  gdufLessonUrl: "http://www.gduf-api.com/lesson/get-lesson",//查看课表接口
-  gdufGetBookListUrl: "http://www.gduf-api.com/book/get-book-list",//查询图书接口
-  gdufGetBookImageUrl: "http://www.gduf-api.com/book/get-book-image",//查询图书图片接口
-  gdufgetBookDetailUrl: "http://www.gduf-api.com/book/get-book-detail",//查询图书详情接口
-  gdufgetCurrentFeeUrl: "http://www.gduf-api.com/fee/get-current-fee",//查询当前水电费接口
-  gdufgetFeeListUrl: "http://www.gduf-api.com/fee/get-fee-list",//查询水电费详情接口
+  gdufLoginUrl: host + "/site/login",//登录接口
+  gdufScoreUrl: host + "/site/score",//查询成绩接口
+  gdufRoomUrl: host + "/room/get-room",//查询自习室接口
+  gdufLessonUrl: host + "/lesson/get-lesson",//查看课表接口
+  gdufGetBookListUrl: host + "/book/get-book-list",//查询图书接口
+  gdufGetBookImageUrl: host + "/book/get-book-image",//查询图书图片接口
+  gdufgetBookDetailUrl: host + "/book/get-book-detail",//查询图书详情接口
+  gdufgetCurrentFeeUrl: host + "/fee/get-current-fee",//查询当前水电费接口
+  gdufgetFeeListUrl: host + "/fee/get-fee-list",//查询水电费详情接口
   scoreFrom: ["全部", "2017-2018-2(第二学期)", "2017-2018-1(第一学期)", "2016-2017-2(第二学期)", "2016-2017-1(第一学期)", "2015-2016-2(第二学期)", "2015-2016-1(第一学期)", "2014-2015-2(第二学期)", "2014-2015-1(第一学期)", "2013-2014-2(第二学期)", "2013-2014-1(第一学期)"],//成绩查询学期列表，每个学期要新增一个
 }
